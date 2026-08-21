f0cc79e4258a510ece2a1a7ea111cdef583c9267
Shipeaseshipping <shipeasee@outlook.com>
2026-08-21 09:43:14 +0100

    Update supabase.js

--- outputs/supabase.js
+++ outputs/supabase.js
@@ -3,3 +3,4 @@ const stylesheet=document.createElement("link");stylesheet.rel="stylesheet";styl
 export const supabase=createClient("https://dtzuagluzfshsvjfeoar.supabase.co","sb_publishable_xzHBV3mB6Iwit2rX66TaVA_p55ZqrOg");
 export async function role(){const {data:{user}}=await supabase.auth.getUser();if(!user)return null;const {data}=await supabase.rpc('my_role');return data}
 import './route-enhancements.js';
+import './customer-tracker-fix.js';
